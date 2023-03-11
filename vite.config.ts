e10f506d29b41fa114fbedf08cf782c619be761c
kacperlachowski <kacperlachowsky@gmail.com>
2023-03-11 17:55:06 +0100

    Feat :sparkles: (API): Add ApolloClient configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import replace from "@rollup/plugin-replace";
+import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
+import { join } from "path";
+import { defineConfig } from "vite";
+
+dotenv.config({ path: join(__dirname, ".env") });
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    replace({
+      "process.env.API_URL": JSON.stringify(process.env.API_URL),
+      "process.env.WS_URL": JSON.stringify(process.env.WS_URL),
+    }),
+  ],
+});
